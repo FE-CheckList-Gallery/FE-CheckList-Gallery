84c1e79db26c88965a018306b1231ee3d8ab4b38
minsoo <minsu2344@naver.com>
2022-06-20 21:53:58 +0900

    Fix: console.log 및 오류 제거

--- src/components/gallery/Detail.tsx
+++ src/components/gallery/Detail.tsx
@@ -1,132 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import CodeMirror from '@uiw/react-codemirror';
 import styled from 'styled-components';
 import { Link, useParams } from 'react-router-dom';
-import { postApi } from '../../lib/api';
 import CodeEditor from '@uiw/react-textarea-code-editor';
-import {IAllPostProps} from '../../types/interface';
-
-function Title({ title }) {
-  return (
-    <DetailTitleBox>
-      <h2>{title}</h2>
-      <Line />
-    </DetailTitleBox>
-  );
-}
-
-function Tags({ categories }) {
-  const tagBox = categories; // fetch로 받아와야 함
-  // tagBox span으로 묶기
-  const tagResult = tagBox.map((tag) => <TagLink key={tag.children} to="#">{tag.category.name}</TagLink>);
-
-  return (
-    <TagBox>
-      {tagResult}
-    </TagBox>
-  );
-}
-
-function Body({ description }) {
-  return (
-    <BodyBox>
-      <p>{description}</p>
-    </BodyBox>
-  );
-}
-
-function CodeContainer({ code }) {
-  const [openState, setOpenState] = useState<boolean>(true);
-
-  function handleToggle() {
-    setOpenState((cur) => !cur);
-  }
-
-  function CodeEditors() {
-    const codes = code.map(file => {
-      async function readFile(curFile) {
-        const read = await file.text();
-        return read;
-      }
-      const text = readFile(file); 
-      const type = file.split('.').pop();
-      console.log(text);
-
-      return (
-        <div>
-          <p>{file}</p>
-          <StyledCodeEditor
-            openState={openState}
-            value={text}
-            padding={20}
-            language={type}
-            readOnly
-          />
-        </div>
-      );
-    });
-    return <div>{codes}</div>
-  }
-
-  return (
-    <div>
-      <CodeToggle openState={openState} onClick={handleToggle}>{openState ? '코드 접기' : '코드 보기'}</CodeToggle>
-      <CodeEditors/>
-    </div>
-  );
-}
-
-function Content({ code }) {
-  return (
-    <ContentBox>
-      {code}
-    </ContentBox>
-  );
-}
-
-export default function Detail() {
-  const [post, setPost] = useState<IAllPostProps>({});
-
-  useEffect(() => {
-    async function Data() {
-      const { postId } = useParams();
-      try {
-        const res = await postApi.getPostById(postId);
-        setPost(() => res);
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  }, []);
-
-  return (
-    <DetailContainer>
-      {/* 제목 */}
-      <Title title={post.title} />
-
-      {/* 태그 */}
-      <Tags categories={post.categories} />
-
-      {/* 내용 */}
-      <Body description={post.description} />
-
-      {/* 코드창 */}
-      <CodeContainer code={post.code} />
-
-      {/* 컨텐츠 */}
-      <Content code={post.code} />
-
-    </DetailContainer>
-  );
-}
-
-// const CodeTitle = styled.p`
-//   display: ${}
-// `
+import { postApi } from '../../lib/api';
+import { ICategoryListProps, IAllPostProps } from '../../types/interface';
 
 const StyledCodeEditor = styled(CodeEditor)`
   font-size: 14px;
-  display: ${(props) => props.openState ? 'block' : 'none'};
+  display: ${(props) => (props.openState ? 'block' : 'none')};
   border-radius: 4px;
   background-color: white;
   border: 1px solid ${(props) => props.theme.palette.daydream};
@@ -190,3 +71,106 @@ const DetailContainer = styled.div`
   width: 100%;
   box-sizing: border-box;
 `;
+
+function Title({ title }: any) {
+  return (
+    <DetailTitleBox>
+      <h2>{title}</h2>
+      <Line />
+    </DetailTitleBox>
+  );
+}
+
+function Tags({ category }: ICategoryListProps) {
+  const tagResult = category.map((tag) => <TagLink key={tag.children} to={`/gallery?tag=${tag.category._id}`}>{tag.category.name}</TagLink>);
+
+  return (
+    <TagBox>
+      {tagResult}
+    </TagBox>
+  );
+}
+
+function Body({ description }) {
+  return (
+    <BodyBox>
+      <p>{description}</p>
+    </BodyBox>
+  );
+}
+
+function CodeContainer({ code }) {
+  const [openState, setOpenState] = useState<boolean>(true);
+
+  function handleToggle() {
+
+    setOpenState((cur) => !cur);
+  }
+
+  function CodeEditors() {
+    const codes = code.map((file) => {
+      async function readFile(curFile) {
+        const read = await file.text();
+        return read;
+      }
+      const text = readFile(file);
+      const type = file.split('.').pop();
+
+      return (
+        <div>
+          <p>{file}</p>
+          <StyledCodeEditor
+            openState={openState}
+            value={text}
+            padding={20}
+            language={type}
+            readOnly
+          />
+        </div>
+      );
+    });
+    return <div>{codes}</div>;
+  }
+
+  return (
+    <div>
+      <CodeToggle openState={openState} onClick={handleToggle}>{openState ? '코드 접기' : '코드 보기'}</CodeToggle>
+      <CodeEditors />
+    </div>
+  );
+}
+
+function Content({ code }) {
+  return (
+    <ContentBox>
+      {code}
+    </ContentBox>
+  );
+}
+
+export default function Detail() {
+  const [post, setPost] = useState<IAllPostProps>({});
+  async function Data() {
+    const { postId } = useParams();
+    try {
+      const res = await postApi.getPostById(postId);
+      setPost(res);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    Data();
+  }, []);
+
+  return (
+    <DetailContainer>
+      <Title title={post.title} />
+      <Tags category={post.categories} />
+      <Body description={post.description} />
+      <CodeContainer code={post.code} />
+      <Content code={post.code} />
+    </DetailContainer>
+  );
+}
